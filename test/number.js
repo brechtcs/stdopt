949b1a90ad67150d8cfe78c2660dee11686eceec
Brecht Savelkoul <brecht.savelkoul@alumni.lse.ac.uk>
2020-05-14 19:15:12 +0200

    Add test for positive and negative numbers

--- test/number.js
+++ test/number.js
@@ -7,6 +7,8 @@ test('number', t => {
   t.equal(number(new Number(5)).value(), 5) // eslint-disable-line
   t.equal(number('blarf').or(5).value(), 5)
   t.equal(number('5').value(), 5)
+  t.equal(number('-5').value(), -5)
+  t.equal(number('+5').value(), 5)
   t.throws(() => number(null).value(), /Value null cannot be parsed as number/)
   t.throws(() => number(false).value(), /Value false cannot be parsed as number/)
   t.throws(() => number(true).value(), /Value true cannot be parsed as number/)
